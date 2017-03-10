72da3f445db1c8e7337f087675e5f8a40448f30d
suanmb <suanmb@outlook.com>
2017-03-10 17:36:55 +0800

    test java

---
+++ src/main/java/baseforjava/GenericTest.java
@@ -0,0 +1,33 @@
+package baseforjava;
+
+import java.util.*;
+
+public class GenericTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		List list=new ArrayList();
+//		list.add(10);
+//		list.add("string");
+//		list.add(10.11111);
+//		for(int i=0;i<list.size();i++)
+//			System.out.println(list.get(i));
+		
+		
+		
+		//使用泛型
+		List <String> list=new ArrayList<String>();
+		list.add("string1");
+		list.add("string2");
+		//list.add(10);
+		for(int i=0;i<list.size();i++){
+			String name = list.get(i);
+			System.out.println("name:"+name);
+		}
+	}
+
+}
